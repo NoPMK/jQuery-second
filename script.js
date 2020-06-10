bc429906d8d7fe7a69b4fb0a74e21ef67c724675
NoPMK <gacsdan@gmail.com>
2020-06-10 18:37:56 +0200

    Done

---
+++ script.js
@@ -0,0 +1,22 @@
+let color = "purple";
+let number = 10;
+let word = "coole";
+
+if (color === "purple"){
+$(".red").css("background-color" , "purple")};
+
+if (number > 100){
+    $(".yellow").html("whoah, that's a big number.")
+ } else { 
+    $(".yellow").html("just a regular number, please.")
+};
+
+if (word === "cool") {
+    $(".light").html("Power of DOM")
+} else {
+    $(".blue").html("Power of DOM")
+};
+
+
+
+
